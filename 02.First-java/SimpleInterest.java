42d3bfd157c2e351a7709a82ac1640a5aa9c7358
Arnapurna Bedi <bediarnapurna20@gmail.com>
2021-09-23 21:00:27 +0530

    Calculate simple interest

--- 02.First-java/SimpleInterest.java
+++ 02.First-java/SimpleInterest.java
@@ -1,3 +1,5 @@
+// Write a program to input principle, time, and rate (P, T, R) from the user and find Simple Interest.
+
 
 import java.util.Scanner;
 
@@ -6,7 +8,7 @@ public class SimpleInterest{
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        
+
         System.out.print("Enter principle amount : ");
         int principle = input.nextInt();
         System.out.print("Enter time : ");
