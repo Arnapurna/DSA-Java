ae41d20efa2f5d80a9716b3e12d0dceebbacb2e4
Arnapurna Bedi <bediarnapurna20@gmail.com>
2021-09-23 18:54:39 +0530

    Greeting message for a name

---
+++ 02.First-java/Greeting.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Greeting{
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+    //  String name = "Arnapurna";
+
+        System.out.println("Namaste " + name +"!");
+    }
+}
