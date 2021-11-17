ca2f638660d766a8899fcfc87d934aadcda412d2
Arnapurna Bedi <bediarnapurna20@gmail.com>
2021-11-17 20:55:14 +0530

    Perimeter Of Circle

---
+++ 03.Conditionals-Loops/PerimeterOfCircle.java
@@ -0,0 +1,14 @@
+// Find Perimeter Of Circle
+
+import java.util.Scanner;
+
+public class PerimeterOfCircle{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the radius :");
+        double radius = in.nextDouble();
+        double perimeter = 2 * (3.14) * radius;
+        System.out.println("Perimeter Of Circle is "+perimeter);
+    }
+}
