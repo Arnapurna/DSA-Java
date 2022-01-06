987b6e0a2cfea5d64ef92d0573fdae94e259173f
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-01-06 15:55:42 +0530

    Perimeter Of Square

---
+++ 03.Conditionals-Loops/PerimeterOfSquare.java
@@ -0,0 +1,16 @@
+// Perimeter Of Square
+
+import java.util.Scanner;
+
+public class PerimeterOfParallelogram{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("Enter the side :");
+        double side = in.nextDouble();
+       
+        double perimeter = 4 * side;
+        System.out.println("Perimeter Of Square is "+perimeter);
+    }
+}
