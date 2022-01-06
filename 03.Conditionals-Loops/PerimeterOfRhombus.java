63e2d3f1d2765b5e60798f6f296a84a7ec3901bb
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-01-06 15:59:35 +0530

    Perimeter Of Rhombus

---
+++ 03.Conditionals-Loops/PerimeterOfRhombus.java
@@ -0,0 +1,16 @@
+// Perimeter Of Rhombu
+
+import java.util.Scanner;
+
+public class PerimeterOfRhombus{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        // System.out.println("Enter the side :");
+        // double side = in.nextDouble();
+       double side = 6;
+        double perimeter = 4 * side;
+        System.out.println("Perimeter Of Rhombus is "+perimeter);
+    }
+}
