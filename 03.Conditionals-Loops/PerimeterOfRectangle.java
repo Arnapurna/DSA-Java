e5713baae9bf1568a36cc3faa86e33933c9558a1
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-01-06 16:34:36 +0530

    Perimeter Of Rectangle

--- 03.Conditionals-Loops/PerimeterOfRectangle.java
+++ 03.Conditionals-Loops/PerimeterOfRectangle.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class PerimeterOfParallelogram{
+public class PerimeterOfRectangle{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
