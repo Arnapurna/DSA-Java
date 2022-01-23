521aabc80f023ffec60f38a0b94da8bf80f48abb
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-01-23 22:11:12 +0530

    Calculate Power

--- 03.Conditionals-Loops/Power.java
+++ 03.Conditionals-Loops/Power.java
@@ -7,8 +7,6 @@ public class Power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a= 2;
-        int b= 3;
 
         System.out.println("Enter two numbers : ");
         // int a = sc.nextInt();
